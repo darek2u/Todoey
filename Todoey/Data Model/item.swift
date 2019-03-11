2f023866ff757fc21bca0f7d8d0c5e5e8409f62e
Dariusz Urbanczyk <dariuszurbanczyk@dariuszs-macbook-air.local>
2019-03-11 23:21:44 +0000

    Convert to MVC design pattern

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,13 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Dariusz Urbanczyk on 11/03/2019.
+//  Copyright © 2019 Dariusz Urbanczyk. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
